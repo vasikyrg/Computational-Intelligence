5597b239a4bb16172e16cc87aba3589642a7a6a4
Vasilis Kyrgyrides <95449708+vasikyrg@users.noreply.github.com>
2022-01-24 21:05:55 +0200

    Update MLP_MNIST_dataset.py

--- Chapter_3/MLP_MNIST_dataset.py
+++ Chapter_3/MLP_MNIST_dataset.py
@@ -9,19 +9,17 @@ import matplotlib.pyplot as plt
 from keras.initializers import RandomNormal
 import time
 
-# --> Διαχείρηση Δεδομένων
-# Κατεβαίνει το dataset της εκφώνησης, εξασφαλίζεται η τυχαιότητα
+
 (Trnx, Trny), (Tstx, Tsty) = mnist.load_data()
 
-# Μετατροπή συστοιχιών κλάσης 1 διαστάσεων σε πίνακες κλάσης 10 διαστάσεων για την είσοδο
 Trny = np_utils.to_categorical(Trny, 10)
 Tsty = np_utils.to_categorical(Tsty, 10)
 
-# Μετατροπή συστοιχιών κλάσης 1 διαστάσεων σε πίνακες κλάσης 10 διαστάσεων για την έξοδο
-Trnx = Trnx / 255.0  # Γενικά παίρνει τιμές από 1 έως 255 όμως εμείς θέλουμε από [0,1]
-Tstx = Tstx / 255.0  # Γενικά παίρνει τιμές από 1 έως 255 όμως εμείς θέλουμε από [0,1]
 
-# Κάνουμε διαχωρισμό με σκοπό να έχουμε το 20% για επικύρωση
+Trnx = Trnx / 255.0  # [0,1]
+Tstx = Tstx / 255.0  # [0,1]
+
+
 Trnx, valx = tf.split(Trnx, [int(48000), int(12000)], 0)
 Trny, valy = tf.split(Trny, [int(48000), int(12000)], 0)
 
@@ -33,7 +31,7 @@ regular_l1 = [l1(0.01)]
 r_c = [0.1, 0.01, 0.001, 0.01]
 r_c_n = [2, 2, 2, 1]
 times = []
-# # --> Δημιουργία default μοντέλου
+
 # for b1 in range(0, len(batching)):
 #     start = time.time()
 #     print("At this moment batch size is ", batching[b1], "")
@@ -45,7 +43,7 @@ times = []
 #     model1.compile(loss=tf.keras.losses.CategoricalCrossentropy(), metrics=[tf.keras.metrics.CategoricalAccuracy()])
 #     history1 = model1.fit(Trnx, Trny, batch_size=batching[b1], epochs=100, validation_data=(valx, valy))
 #
-#     # Εκτύπωση Διαγραμμάτων και Αποθήκευση τους
+#     
 #     trn_acc = [history1.history['categorical_accuracy'][i] * 100 for i in range(100)]
 #     val_acc = [history1.history['val_categorical_accuracy'][i] * 100 for i in range(100)]
 #     trn_los = history1.history['loss']
@@ -76,7 +74,7 @@ times = []
 #     times.append((time.time()-start))
 #     print("Time for batch = " + str(batching[b1]) + " is "+str(times[b1])+" sec")
 #
-# # Δημιουργία μοντέλου RMSprop με βάση τις προδιαγρφές που λέει η εκφώνηση
+# 
 #
 # for r in range(0, len(rho)):
 #     print("At this moment rho is ", rho[r], "")
@@ -115,7 +113,7 @@ times = []
 #     plt.savefig('Losses with rho= ' + str(rho[r]) + '.png')
 #     plt.close()
 #
-# # Δημιουργία μοντέλου SGD με βάση τις προδιαγρφές lr =0.01 με μ.ο 10
+# 
 #
 #     print("At this moment regularization choice is lr=0.01 with W = 10")
 #     model2 = Sequential()
@@ -128,7 +126,7 @@ times = []
 #                    metrics=[tf.keras.metrics.CategoricalAccuracy()])
 #     history2 = model2.fit(Trnx, Trny, batch_size=256, epochs=100, validation_data=(valx, valy))
 #
-#     # Εκτύπωση Διαγραμμάτων και Αποθήκευση τους
+#     
 #     trn_acc = [history2.history['categorical_accuracy'][i] * 100 for i in range(100)]
 #     val_acc = [history2.history['val_categorical_accuracy'][i] * 100 for i in range(100)]
 #     trn_los = history2.history['loss']
@@ -154,7 +152,7 @@ times = []
 #     plt.savefig('(SGD) Losses with lr=0.01 with W = 10 .png')
 #     plt.close()
 #
-# # Δημιουργία μοντέλου SGD με βάση τις προδιαγρφές lr =0.01 με μ.ο 10 και l2-norm
+# 
 #     for reg2 in range(0, len(regular)):
 #         print("At this moment regularization choice is l", r_c_n[reg2], " with a = ", r_c[reg2], "")
 #         model2 = Sequential()
@@ -170,7 +168,7 @@ times = []
 #                        metrics=[tf.keras.metrics.CategoricalAccuracy()])
 #         history2 = model2.fit(Trnx, Trny, batch_size=256, epochs=100, validation_data=(valx, valy))
 #
-#         # Εκτύπωση Διαγραμμάτων και Αποθήκευση τους
+#         
 #         trn_acc = [history2.history['categorical_accuracy'][i] * 100 for i in range(100)]
 #         val_acc = [history2.history['val_categorical_accuracy'][i] * 100 for i in range(100)]
 #         trn_los = history2.history['loss']
@@ -196,7 +194,7 @@ times = []
 #         plt.savefig('(SGD) Losses with regularization= l' + str(r_c_n[reg2]) + ' with a=' + str(r_c[reg2]) + '.png')
 #         plt.close()
 
-# Δημιουργία μοντέλου SGD με βάση τις προδιαγρφές lr =0.01 με μ.ο 10 και l1-norm και με dropout(0.3)
+
 print("At this moment regularization choice is l1-norm with a = 0.01")
 model2 = Sequential()
 model2.add(Flatten(input_shape=(28, 28)))
@@ -209,7 +207,7 @@ model2.compile(optimizer=tf.keras.optimizers.SGD(), loss=tf.keras.losses.Categor
                metrics=[tf.keras.metrics.CategoricalAccuracy()])
 history2 = model2.fit(Trnx, Trny, batch_size=256, epochs=100, validation_data=(valx, valy))
 
-# Εκτύπωση Διαγραμμάτων και Αποθήκευση τους
+
 trn_acc = [history2.history['categorical_accuracy'][i] * 100 for i in range(100)]
 val_acc = [history2.history['val_categorical_accuracy'][i] * 100 for i in range(100)]
 trn_los = history2.history['loss']
